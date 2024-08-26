68f387d5cb5b58408589576ac8ce32ac667a4b1f
Michael Cloran <michaelcloran2010@gmail.com>
2024-08-26 09:11:59 +0000

    added add to bag functionality

--- fitness_fanatic/settings.py
+++ fitness_fanatic/settings.py
@@ -157,12 +157,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = ( os.path.join(BASE_DIR, 'static'), )
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-FREE_DELIVERY_THRESHOLD = 25
+FREE_DELIVERY_THRESHOLD = 30
 STANDARD_DELIVERY_PERCENTAGE = 10
 
 # Default primary key field type
