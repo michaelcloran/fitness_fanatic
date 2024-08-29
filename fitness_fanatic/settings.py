a0dc8b68455892524a117a5585df01a5fcc979fb
Michael Cloran <michaelcloran2010@gmail.com>
2024-08-29 12:55:37 +0000

    added checkout views and templates

--- fitness_fanatic/settings.py
+++ fitness_fanatic/settings.py
@@ -51,6 +51,9 @@ INSTALLED_APPS = [
     'bag',
     'checkout',
     'profiles',
+
+    'crispy_forms',
+    'crispy_bootstrap4',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +69,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'fitness_fanatic.urls'
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -80,8 +85,13 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
                 'bag.contexts.bag_contents',
             ],
+            'builtins': [
+                'crispy_forms.templatetags.crispy_forms_tags',
+                'crispy_forms.templatetags.crispy_forms_field',
+            ]
         },
     },
 ]
