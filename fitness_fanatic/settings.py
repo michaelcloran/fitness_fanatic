56191933f35071affc08ff98d2b47dfd71fe80ed
Michael Cloran <michaelcloran2010@gmail.com>
2024-09-22 02:28:31 +0000

    setup development and production for debug flag in settings

--- fitness_fanatic/settings.py
+++ fitness_fanatic/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['8000-michaelclor-fitnessfana-t24i36frcr9.ws.codeinstitute-ide.net','fitness-fanatic-8422c1e8a4c9.herokuapp.com']
 
