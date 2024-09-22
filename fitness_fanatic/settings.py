880d7c43d757bd778bc0808aec080528c9eb1f75
Michael Cloran <michaelcloran2010@gmail.com>
2024-09-22 04:44:59 +0000

    updated settings heroku not finding static css

--- fitness_fanatic/settings.py
+++ fitness_fanatic/settings.py
@@ -179,6 +179,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [ os.path.join(BASE_DIR, 'static'), ]
 
 if 'USE_AWS' in os.environ:
     # Cache control
