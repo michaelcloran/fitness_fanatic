c9d6531117fc1c9065a932a47873c7fba5a987c6
Michael Cloran <michaelcloran2010@gmail.com>
2024-10-08 13:50:27 +0000

    fixed bug view trainers and add workout only visible to admin or trainers

--- home/views.py
+++ home/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
-
+from trainers.models import TrainerProfile
 
 # Create your views here.
+def check_trainer_user_exists(username):
+    if TrainerProfile.objects.filter(user=username).exists():
+        return True
+    return False
+
+
 def index(request):
     """ A view to retuen the index page """
+    if request.user.is_authenticated:
+        trainer_bool = check_trainer_user_exists(request.user)
+    else:
+        trainer_bool = False
 
-    return render(request, 'home/index.html')
+    context = {
+       'is_trainer_bool': trainer_bool,
+    }
+    return render(request, 'home/index.html', context)
